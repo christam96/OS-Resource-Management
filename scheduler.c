4a6c19353e83f6da987a2e5766c771fd46ce927b
Christopher Tam <christam96>
2018-11-26 00:00:57 -0500

    commit

--- scheduler.c
+++ scheduler.c
@@ -16,15 +16,16 @@ job_t *get_next_job(int mode, d_linked_list_t* jobs) {
 
 	if (mode == 0) {
 		printf("Then mode is FCFS\n");
-		for (int i = 0; i < 6; i++) {
+		printf("Size of jobs linked list is: %d \n", jobs->size);
+		/**
+		for (int i = 0; i < jobs->size; i++) {
 			j = (job_t*) jobs->head;
-			//jobs->dequeue(jobs->head);
 			jobNumber = j->number;
 			required_mem = j->required_memory;
 			required_time = j->required_time;
 
 			printf("%d %d %d \n", jobNumber, required_mem, required_time);
-
+**/
 		}
 	}
 
