7c4ea2ca340169023f182b73816ac60f56e2a61b
Christopher Tam <christam96>
2018-11-26 13:14:41 -0500

    commit

--- scheduler.c
+++ scheduler.c
@@ -13,19 +13,16 @@ job_t *get_next_job(int mode, d_linked_list_t* jobs) {
 	job_t *j;
 
 	pthread_mutex_t mutex1 = PTHREAD_MUTEX_INITIALIZER;
-	Bool finish = true;
 
-	for (int i=0;i<jobs->size;i++) {
-		// Ensures FCFS mode dequeues jobs in correct order
-	}
+	int counter = 0;
 
 	// Mode 0: First Come First Serve (FCFS)
 	//
 	if (mode == 0 && finish == true) {
-			pthread_mutex_lock(&mutex1);
-			finish = false;
-			j = (job_t*) dequeue(jobs);
-			pthread_mutex_unlock(&mutex1);
+
+
+			j = (job_t*) dequeue(jobs[i]);
+
 		/**
 		printf("Then mode is FCFS\n");
 		for (int i = 0; i < jobs->size; i++) {
