3ff8bea9d1ffcc355b30ba7c74e83d3d5c632adb
Christopher Tam <christam96>
2018-11-26 14:28:56 -0500

    commit

--- scheduler.c
+++ scheduler.c
@@ -15,21 +15,7 @@ job_t *get_next_job(int mode, d_linked_list_t* jobs) {
 	// Mode 0: First Come First Serve (FCFS)
 	//
 	if (mode == 0) {
-
-
 			j = (job_t*) dequeue(jobs);
-
-		/**
-		printf("Then mode is FCFS\n");
-		for (int i = 0; i < jobs->size; i++) {
-			j = (job_t*) dequeue(jobs);
-			jobNumber = j->number;
-			required_mem = j->required_memory;
-			required_time = j->required_time;
-
-			printf("%d %d %d \n", jobNumber, required_mem, required_time);
-		}
-		**/
 	}
 
 	if (mode == 1) {
