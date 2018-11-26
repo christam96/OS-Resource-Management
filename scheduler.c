11ae1bce68c2b81f17d882c0d62f211488cfbce5
Christopher Tam <christam96>
2018-11-26 15:16:48 -0500

    commit

--- scheduler.c
+++ scheduler.c
@@ -9,22 +9,30 @@
 
 #include "scheduler.h"
 
+int countJobsScheduled = 0;
+
 job_t *get_next_job(int mode, d_linked_list_t* jobs) {
 	job_t *j;
+
+	if (countJobsScheduled == jobs->size) {
+		j = NULL;
+	}
 
 	// Mode 0: First Come First Serve (FCFS)
 	//
 	if (mode == 0) {
 			j = (job_t*) dequeue(jobs);
+			countJobsScheduled++;
 	}
 
 	// Mode 1: Last In First Out (LIFO)
 	if (mode == 1) {
 			j = (job_t*) pop(jobs);
+			countJobsScheduled++;
 	}
 
 	// Mode 2: Shortest Job First (SJF)
-	
+
 
 
 
