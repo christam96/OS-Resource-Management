7db568902e0953fb0df75e9a144df278779af6d1
Christopher Tam <christam96>
2018-12-02 18:57:00 -0500

    commit

--- scheduler.c
+++ scheduler.c
@@ -28,7 +28,7 @@ job_t *get_next_job(int mode, d_linked_list_t* jobs) {
 	if (mode == 2) {
 		tempJob1 = (job_t*) dequeue(jobs);
 		for (int i=0; i<jobs->size; i++) {
-			printf("size: %d", jobs->size)
+			printf("size: %d", jobs->size);
 			tempJob2 = (job_t*) dequeue(jobs);
 			if (tempJob2->required_time < tempJob1->required_time ) {
 				enqueue(jobs, tempJob1);
