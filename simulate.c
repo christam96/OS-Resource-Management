6d0756ec5a7c4cc816ac3562424c961eef8e2bb7
Christopher Tam <christam96>
2018-12-02 22:41:50 -0500

    commit

--- simulate.c
+++ simulate.c
@@ -23,7 +23,7 @@ void* run(void *j)
 	**************************************************************************/
 	pthread_mutex_lock(&lock);
 	job_t *job = get_next_job(mode, jobs);
-	pthread_mutex_unlock(&lock);
+	//pthread_mutex_unlock(&lock);
 	int number, required_memory;
 	//pthread_mutex_t lock;
 
@@ -62,7 +62,7 @@ void* run(void *j)
 			enqueue(jobs, job);
 		}
 		// Critical region!!!
-		pthread_mutex_lock(&lock);
+		//pthread_mutex_lock(&lock);
 		job = get_next_job(mode, jobs);
 		pthread_mutex_unlock(&lock);
 	}
